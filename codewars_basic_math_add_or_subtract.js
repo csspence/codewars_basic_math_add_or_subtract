cdd4e36d4931c1598909bfdd19bfc5babd803571
Crew Spence <csspence123@gmail.com>
2021-05-08 06:01:16 -0500

    Working solution.

--- codewars_basic_math_add_or_subtract.js
+++ codewars_basic_math_add_or_subtract.js
@@ -9,51 +9,18 @@ Examples
 */
 
 const calculate = (str) => {
-  const signs = [];
-  const nums = [];
-  let sum = 0;
-  let arr;
-  let negative = false;
-  const numberMaker = (index, string) => {
-    let numString = '';
-    let ind = index;
-    for(let i = 0; i < string.length; i++) {
-      if((Number(string[i]) * -1) < 0 ) {
-        numString += string[i];
-        ind = i;
-      } else {
+  let numbers = str.split(/plus|minus/);
+  let operations = str.match(/[a-z]+/g);
+  let sum = parseInt(numbers[0]);
+  for(let i = 0, len = operations.length; i < len; i++){
+    switch(operations[i]){
+      case "plus":
+        sum += parseInt(numbers[i+1]);
         break;
-      }
-      ind = i;
-    }
-
-    return [numString, ind];
-  };
-
-
-  for(let i = 0; i < str.length; i++) {
-    if(str[i] === 'p') {
-      signs.push('+');
-    }
-    if(str[i] === 'm') {
-      signs.push('-');
-    }
-    if(str[i] === '-') {
-      negative = true;
-    }
-    if(Number(str[i]) * 0 === 0) {
-      arr = numberMaker(i, str);
-      nums.push(arr[0]);
-      i = arr[1] - 1;
+      case "minus":
+        sum -= parseInt(numbers[i+1]);
+        break;
     }
   }
-  sum = nums[0];
-  for(let i = 1; i < nums.length; i++) {
-    if(signs[i - 1] === '+') {
-      sum += nums[i];
-    } else {
-      sum -= nums[i];
-    }
-  }
-  return sum.toString();
+  return sum+"";
 }
